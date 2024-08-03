b79d6694175d8486d210994dd1938edae971ca73
Yahampath Chandika <yhmpth@gmail.com>
2024-08-03 12:11:39 +0530

    eventItems

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,8 @@ const itemRoutes = require("./items.routes");
 const itemUsageRoutes = require("./itemUsage.routes");
 const customerRoutes = require("./customer.routes");
 const eventRoutes = require("./event.routes")
-const orderRoutes = require("./order.routes")
+const orderRoutes = require("./order.routes");
+const eventItemsRoutes = require("./eventItems.routes");
 function routes() {
   const router = express.Router();
 
@@ -15,6 +16,7 @@ function routes() {
   router.use("/itemUsage", itemUsageRoutes);
   router.use("/event", eventRoutes);
   router.use("/order", orderRoutes);
+  router.use("/eventItems", eventItemsRoutes);
 
   return router;
 }
